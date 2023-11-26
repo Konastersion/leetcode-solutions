1b27b78485e435273ba810a55473543611562a0f
Konastersion <konastersion@gmail.com>
2023-11-26 21:12:01 +0400

    Update valid-anagram.js

--- valid-anagram.js
+++ valid-anagram.js
@@ -7,8 +7,6 @@ Given two strings s and t, return true if t is an anagram of s, and false otherw
 
 An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 
- 
-
 Example 1:
 
 Input: s = "anagram", t = "nagaram"
@@ -25,10 +23,6 @@ Constraints:
 s and t consist of lowercase English letters.
 
 */
-
-
-
-
 
 let s ='anagram';
 
